d0ebd1e1a5a7e5ec20d50efc34d1554ce693fe11
Prolifesoft <ncem0332006@gmail.com>
2025-08-21 17:16:30 +0300

    Add files via upload

---
+++ __manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'Veresiye Defteri',
+    'version': '17.0.1.0.0',
+    'summary': 'Müşteri veresiye takibi',
+    'author': 'Your Company',
+    'category': 'Accounting',
+    'depends': ['base', 'product', 'account'],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/ledger_views.xml',
+        'views/payment_wizard_views.xml',
+        'report/receipt_report.xml',
+    ],
+    'installable': True,
+    'application': True,
+}
---
+++ wizard/payment_wizard.py
@@ -0,0 +1,16 @@
+from odoo import api, fields, models
+
+
+class VeresiyePaymentWizard(models.TransientModel):
+    _name = 'veresiye.payment.wizard'
+    _description = 'Ödeme Yap'
+
+    ledger_id = fields.Many2one('veresiye.defteri', string='Defter', required=True)
+    amount = fields.Monetary(string='Ödeme Tutarı', required=True)
+    currency_id = fields.Many2one('res.currency', related='ledger_id.currency_id', readonly=True)
+
+    def action_confirm(self):
+        self.ensure_one()
+        ledger = self.ledger_id
+        ledger.paid_amount += self.amount
+        return {'type': 'ir.actions.act_window_close'}
