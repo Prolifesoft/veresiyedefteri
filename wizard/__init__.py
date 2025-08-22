f1156bac2b9fda197e0849d9db512117e9e6e837
Prolifesoft <ncem0332006@gmail.com>
2025-08-23 02:12:06 +0300

    Fix receipt report reference and show line totals

--- wizard/__init__.py
+++ wizard/__init__.py
@@ -1,1 +1,1 @@
-from . import payment_wizard
+from . import payment_wizard  # noqa: F401
--- wizard/payment_wizard.py
+++ wizard/payment_wizard.py
@@ -1,13 +1,17 @@
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class VeresiyePaymentWizard(models.TransientModel):
     _name = 'veresiye.payment.wizard'
     _description = 'Ödeme Yap'
 
-    ledger_id = fields.Many2one('veresiye.defteri', string='Defter', required=True)
+    ledger_id = fields.Many2one(
+        'veresiye.defteri', string='Defter', required=True
+    )
     amount = fields.Monetary(string='Ödeme Tutarı', required=True)
-    currency_id = fields.Many2one('res.currency', related='ledger_id.currency_id', readonly=True)
+    currency_id = fields.Many2one(
+        'res.currency', related='ledger_id.currency_id', readonly=True
+    )
 
     def action_confirm(self):
         self.ensure_one()
